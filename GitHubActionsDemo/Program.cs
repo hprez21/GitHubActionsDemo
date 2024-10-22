a41c3e0fd1a021c3ba5b502062a7dccf6a47afa0
Héctor Pérez <hprez21@users.noreply.github.com>
2024-10-22 11:55:19 -0600

    Update Program.cs

--- GitHubActionsDemo/Program.cs
+++ GitHubActionsDemo/Program.cs
@@ -1,14 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitHub learners!");
-
 Console.WriteLine("Creating txt file");
 
-//Create a new file in the current directory.
-using (FileStream fs = File.Create("test.txt"))
+// Get the directory where the executable is located
+string executingPath = AppContext.BaseDirectory;
+string filePath = Path.Combine(executingPath, "test.txt");
+
+Console.WriteLine($"Creating file at: {filePath}");
+
+// Create the file in the same directory as the executable
+using (FileStream fs = File.Create(filePath))
 {
     Byte[] info = new System.Text.UTF8Encoding(true).GetBytes("This is some text in the file.");
     // Add some information to the file.
-    fs.Write(info, 0, info.Length);
+    fs.Write(info, 0, info.Length); 
 }
-
 Console.WriteLine("File created successfully");
